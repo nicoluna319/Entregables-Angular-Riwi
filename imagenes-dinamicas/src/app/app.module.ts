914914ede1ec2647a93b1d4808a2706e1c15aaa1
nluna319 <nicolaslunaromero@gmail.com>
2024-09-16 17:55:28 -0500

    Primer commit

---
+++ imagenes-dinamicas/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ImageDetailsComponent } from './image-details/image-details.component';
+import { CategoryFilterComponent } from './category-filter/category-filter.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GalleryComponent,
+    ImageDetailsComponent,
+    CategoryFilterComponent,
+    FavoritesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
